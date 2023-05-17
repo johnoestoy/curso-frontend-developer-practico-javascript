41dd331ac0ac757781315a907357252f74c6046e
John <johnoestoy@gmail.com>
2023-05-17 16:40:40 +0100

    Create mobile menu, style css and create inactive function

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navBar = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
-navBar.addEventListener('click', toggleDesktop);
+const iconMenu = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu')
+
 
 function toggleDesktop(){
   desktopMenu.classList.toggle('inactive')
 }
+function toggleMobile(){
+  menuMobile.classList.toggle('inactive')
+}
+navBar.addEventListener('click', toggleDesktop);
+iconMenu.addEventListener('click', toggleMobile);
