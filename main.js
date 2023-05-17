155bf66e73fd1547f15eaa16c7235f8482488759
John <johnoestoy@gmail.com>
2023-05-17 18:26:30 +0100

    Add shopping cart aside

--- main.js
+++ main.js
@@ -4,12 +4,41 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const iconMenu = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu')
 
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
-function toggleDesktop(){
+function toggleProductDetail(){
+  const isDesktopMenuClose = desktopMenu.classList.contains('inactive');
+  const isMenuMobileClose = menuMobile.classList.contains('inactive');
+
+  if(!isMenuMobileClose){
+    menuMobile.classList.add('inactive')
+  }
+  if(!isDesktopMenuClose){
+    desktopMenu.classList.add('inactive')
+  }
+  productDetail.classList.toggle('inactive')
+}
+
+function toggleMenuDesktop(){
+  const isProductDetailClose = productDetail.classList.contains('inactive');
+  if(!isProductDetailClose){
+    productDetail.classList.add('inactive')
+  }
+
   desktopMenu.classList.toggle('inactive')
 }
-function toggleMobile(){
+function toggleMenuMobile(){
+  const isProductDetailClose = productDetail.classList.contains('inactive')
+
+  if (!isProductDetailClose){
+    productDetail.classList.add('inactive')
+  }
+
   menuMobile.classList.toggle('inactive')
 }
-navBar.addEventListener('click', toggleDesktop);
-iconMenu.addEventListener('click', toggleMobile);
+
+
+navBar.addEventListener('click', toggleMenuDesktop);
+iconMenu.addEventListener('click', toggleMenuMobile);
+shoppingCart.addEventListener('click', toggleProductDetail);
